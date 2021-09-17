e16a1c468d9ce77721a72228a7b104b65774d1d1
hayleethegamer <18407070+hayleethegamer@users.noreply.github.com>
2021-09-17 20:56:55 +0000

    Create MacroCommands.py

---
+++ MacroCommands.py
@@ -0,0 +1,14 @@
+#Example of some Macros
+def defComs(dev, parent):
+	@parent.command("KEY_UP", pressType="hold")
+	async def volumeUp(pressType):
+		parent.keyPress("XF86AudioRaiseVolume")
+	@parent.command("KEY_DOWN", pressType="hold")
+	async def volumeUp(pressType):
+		parent.keyPress("XF86AudioLowerVolume")
+	@parent.command("KEY_M", pressType="hold")
+	async def volumeUp(pressType):
+		parent.keyPress("XF86AudioMute")
+	@parent.command("KEY_C")
+	async def calcuator(pressType):
+		parent.exeCom("gnome-calculator")
