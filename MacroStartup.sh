07ad887acc574565b91d4790462e17f6698b821c
hayleethegamer <18407070+hayleethegamer@users.noreply.github.com>
2021-09-17 20:56:15 +0000

    Create MacroStartup.sh

---
+++ MacroStartup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+userID=$(id -u)
+cd "$HOME" || exit
+while true; do
+	sudo python3 /opt/KeyboardMacro/Macros.py "$userID"
+	if zenity --question --title="Restart Macros?" --text="The Macro Program has stopped, do you wish to restart it?"  --width=200
+	then
+		zenity --info --title="Restarting..." --text="Macro Program is restarting."
+		continue
+	else
+		zenity --info --title="Exiting..." --text="Macro Program is exiting."
+		exit
+	fi
+done
